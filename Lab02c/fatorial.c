c519d3c11c6dc1bec613348fd44d22c0307c5cc8
Victor Cunha <93205770+VictorCunhaG@users.noreply.github.com>
2022-03-14 14:41:19 -0300

    Update fatorial.c

--- Lab02c/fatorial.c
+++ Lab02c/fatorial.c
@@ -10,44 +10,33 @@ fontes:
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc , char *argv[])
-{
+int main(int argc , char *argv[]){
 	pid_t pid; 
-	if (argc != 2) 
-	{
+	if (argc != 2) {
 		printf("falta ou excesso de argumentos\n");
 		exit(0);
 	}
-	
-	if ( atoi ( argv[1] ) < 0)
-	{
+	if ( atoi ( argv[1] ) < 0){
 		printf("nao se calcula fatorial de numero negativo. %d", atoi(argv[1]));
 		exit(0);
 	}
 	pid=fork();
-	if ( pid<0 )
-	{
+	if ( pid<0 ){
 		printf("falha ao criar o filho\n");
 		exit(0);
-	}
-	else if ( pid==0 )
-	{
+	} else if ( pid==0 ){
 		int k = 2;
         int i,j,n;
 		n = atoi(argv[1]);
 		int arr[n];
 		arr[0] = 1;
-		for (i=1 ; i<n; i++) 
-		{
+		for (i=1 ; i<n; i++) {
 			arr[i] = arr[i-1]*k;
 			k++;
 		}
         printf("fat(%d) = %d\n", n,arr[n-1]);
 		exit(0);
-	}
-	//Processo pai
-	else
-	{
+	} else{
 		wait(NULL);
 	}
 }
