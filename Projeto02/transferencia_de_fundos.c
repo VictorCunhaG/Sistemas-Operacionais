397fcbaaf54201f85430e5cd532e0f2892f294d6
unknown <pedrocarvalhogit@outlook.com>
2022-05-20 10:47:15 -0300

    commit codigo funcionando

--- Projeto02/transferencia_de_fundos.c
+++ Projeto02/transferencia_de_fundos.c
@@ -6,15 +6,13 @@
 #include <signal.h>
 #include <sched.h>
 #include <stdio.h>
-#include <pthread.h>
 #include <semaphore.h>
 
 // 64kB stack
-#define FIBER_STACK 1024*64
-sem_t (&trava_mutex);
+#define FIBER_STACK 2048*64
+sem_t trava_mutex;
 
 struct c {
-    
     int saldo;
 };
 
@@ -24,14 +22,17 @@ typedef struct c conta;
 conta from, to;
 int valor;
 
+
+
 // THe child will execute this funcition
 int transferencia(void *arg){
-    sem_wait(&trava_mutex);
+    
     if(from.saldo >= valor){
+        sem_wait(&trava_mutex);
         from.saldo -= valor;
-        to.saldo += valor;       
+        to.saldo += valor;
+        sem_post(&trava_mutex);
     }
-    sem_post(&trava_mutex);
     printf("Transferencia concluida com sucesso!\n");
     printf("Saldo de c1: %d\n", from.saldo);
     printf("Saldo de c2: %d\n", to.saldo);
@@ -39,11 +40,12 @@ int transferencia(void *arg){
 }
 
 int main(){
-
+  
     void* stack;
-    pid_t pid;
+    int thread_pid[10];
     int i;
-    sem_init(&trava_mutex);
+    int status;
+    sem_init(&trava_mutex, 0, 1);
 
     // Allocate the stack
     stack = malloc ( FIBER_STACK );
@@ -54,23 +56,21 @@ int main(){
     }
 
     // Todas as contas come�am com saldo 100
-    from.saldo = 100;
+    from.saldo = 1000000;
     to.saldo = 100;
 
     printf( " Transferindo 10 para a conta c2\n" );
     valor = 10;
 
-    for (i=0; i<10; i++){
+        
+  
+    for (i=0; i<100000; i++){
         // Call the clone system call to recreate the child thread
-        pid = clone( &transferencia, (char*) stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
-
-        if ( pid == -1){
-            perror( "clone" );
-            exit(2);
-        }
-      
+        thread_pid[i] = clone( &transferencia, (char*) stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
+  
+    pid_t pid = waitpid(thread_pid[i], &status, 0);
     }
-    wait(NULL);
+    
     // Free the stack
     free( stack );
     printf("Transferencias concluidas e memoria liberada.\n");
